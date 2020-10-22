03b6e7a4dc93f876f343cd6f623467f07c125d94
magela-lopez <71371549+magela-lopez@users.noreply.github.com>
2020-10-22 01:00:35 -0300

    Delete Exercicio40_BD2.sql

--- Exercicio40_BD2.sql
+++
@@ -1,9 +0,0 @@
-USE world;
-
-SELECT city.*, country.Name,  countrylanguage.language FROM country
-LEFT JOIN city ON  country.Code =city.ID
-LEFT JOIN countrylanguage ON country.Code = countrylanguage.CountryCode
-WHERE countrylanguage.language LIKE'Dutch';
-
-/*40. Selecione todas as cidades dos países no qual se fala a língua ‘Dutch’.
-*/
