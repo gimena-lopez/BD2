ae905c89693dc6737ec00cad209f25d659d1f5db
magela-lopez <71371549+magela-lopez@users.noreply.github.com>
2020-10-22 01:03:13 -0300

    Delete Exercicio41_BD2.sql

--- Exercicio41_BD2.sql
+++
@@ -1,5 +0,0 @@
-USE world;
-SELECT country.*,  countrylanguage.language, countrylanguage.IsOfficial FROM country
-INNER JOIN countrylanguage
-ON country.Code = countrylanguage.CountryCode
-WHERE countrylanguage.language LIKE 'Spanish' AND IsOfficial LIKE 'T';
