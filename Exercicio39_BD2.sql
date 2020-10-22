3d356b032559c5cc819ce77adc8dfc31671e374b
magela-lopez <71371549+magela-lopez@users.noreply.github.com>
2020-10-22 01:00:04 -0300

    Delete Exercicio39_BD2.sql

--- Exercicio39_BD2.sql
+++
@@ -1,6 +0,0 @@
-USE world;
-
-SELECT country.*,  countrylanguage.language FROM country
-INNER JOIN countrylanguage
-ON country.Code = countrylanguage.CountryCode
-WHERE countrylanguage.language LIKE'Dutch';
