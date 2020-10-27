69b4fb34786bf1a66c8c6e161ff2846817a395cc
magela-lopez <71371549+magela-lopez@users.noreply.github.com>
2020-10-27 12:44:19 -0300

    Update Exercicio40_BD2.sql

--- exercicios-1/Exercicio40_BD2.sql
+++ exercicios-1/Exercicio40_BD2.sql
@@ -1,9 +1,6 @@
 USE world;
 
-SELECT city.*, country.Name,  countrylanguage.language FROM country
-LEFT JOIN city ON  country.Code =city.ID
-LEFT JOIN countrylanguage ON country.Code = countrylanguage.CountryCode
+SELECT city.*, country.Name,  countrylanguage.language FROM city
+INNER JOIN country ON  city.CountryCode = country.code
+INNER JOIN countrylanguage ON city.CountryCode = countrylanguage.CountryCode
 WHERE countrylanguage.language LIKE'Dutch';
-
-/*40. Selecione todas as cidades dos países no qual se fala a língua ‘Dutch’.
-*/
