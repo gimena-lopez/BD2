d72641dca597d5a2c8eaca8a8cdbe9b47dbbafbc
magela-lopez <71371549+magela-lopez@users.noreply.github.com>
2020-10-27 21:34:49 -0300

    Update Exercicio45_BD2.sql

--- exercicios-1/Exercicio45_BD2.sql
+++ exercicios-1/Exercicio45_BD2.sql
@@ -1,6 +1,6 @@
 USE world;
 
 SELECT country.name, country.continent, country.region, country.population, city.name, city.district, city.population, countrylanguage.language, countrylanguage.IsOfficial FROM country
-LEFT JOIN city ON country.Code = city.CountryCode
-LEFT JOIN countrylanguage ON country.code = countrylanguage.countryCode
+INNER JOIN city ON country.Code = city.CountryCode
+INNER JOIN countrylanguage ON country.code = countrylanguage.countryCode
 WHERE country.Continent LIKE'Europe' AND countrylanguage.IsOfficial LIKE 'T';
